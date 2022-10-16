72f0168ed86b9a6e70e0210be4caac4ace901591
raphtis <angelvelazquez20@gmail.com>
2022-10-16 18:05:05 -0500

    Added update and delete functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -4,12 +4,73 @@ const Goal = require('../models/goalModel')
 // -desc GET goals
 // -route GET /api/goals
 // -access Private
-
 const getGoals = asyncHandler(async (req,res) => {
-  res.status(200).json('Connected')
+  const goals = await Goal.find()
+
+  res.status(200).json(goals)
 })
+
+
+// -desc CREATE goal
+// -route POST /api/goals
+// -access Private
+const createGoal = asyncHandler(async (req,res) => {
+  // validation 
+  if(!req.body.text){
+    res.status(400)
+    throw new Error('Please add a text field.')
+  }
+
+  const goal = await Goal.create({
+    title: req.body.title,
+    text: req.body.text,
+  })
+
+  res.status(200).json(goal)
+})
+
+// -desc UPDATE goals
+// -route PUT /api/goals/:id
+// -access Private
+const updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+
+  if(!goal){
+    res.status(400)
+    throw new Error('Goal could not be found.')
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new:true})
+
+  res.status(200).json(updatedGoal)
+})
+
+// -desc DELETE goals
+// -route DELETE /api/goals/:id
+// -access Private
+const deleteGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+
+  if(!goal){
+    res.status(400)
+    throw new Error('Goal not found.')
+  }
+
+
+  await goal.remove()
+
+  res.status(200).json({ id: req.params.id })
+})
+
+
+// -desc GET goals
+// -route GET /api/goals
+// -access Private
 
 
 module.exports = {
   getGoals,
+  createGoal,
+  updateGoal,
+  deleteGoal
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals } = require('../controllers/goalController')
+const { getGoals, createGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
 
-router.route('/').get(getGoals)
+router.route('/').get(getGoals).post(createGoal)
 
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
