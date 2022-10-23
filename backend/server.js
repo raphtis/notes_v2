a4dbc9397bbe6d473d6005450d9f30fdaf954488
raphtis <angelvelazquez20@gmail.com>
2022-10-23 14:15:39 -0500

    Added user register route

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 
 connectDB();
