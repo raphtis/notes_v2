024388adcaafd83e8f6e73e71aca4f4e346292fa
raphtis <angelvelazquez20@gmail.com>
2022-10-26 22:48:41 -0500

    Created router styles

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom'
+
 
 function Header() {
+  // const navigate = useNavigate();
+
+
   return (
-    <div>Header</div>
+    <header className="header">
+      <div className="logo">
+        <Link to='/'>Goals</Link>
+      </div>
+      <ul>
+        <li>
+          <button className="btn">
+            <FaSignOutAlt /> Logout
+          </button>
+        </li>
+        <li>
+          <Link to='/login'>
+            <FaSignInAlt /> Login
+          </Link>
+        </li>
+        <li>
+          <Link to='/register'>
+            <FaUser /> Register
+          </Link>
+        </li>
+      </ul>
+    </header>
   )
 }
 
